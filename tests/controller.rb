1d4507e6b12a86161bce3a30d1684cfb15426d91
Michael Stahnke <stahnma@ram.stahnkage.com>
2009-07-18 21:49:43 -0500

    Wrote simple unit tests for configuration loading.

---
+++ tests/controller.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+
+require 'yaml'
+
+
+
+def loadConfig(filename)
+    if File.exists?(filename) and File.size(filename) == 0 
+     raise IOError, 'Zero Byte File', caller
+    end 
+    begin
+      config =  YAML::load(File.open(filename))
+    rescue Errno::EACCES => boom
+      raise Errno::EACCES, "Permission denied on #{filename}", caller
+    rescue Errno::ENOENT => boom
+      raise IOError, boom, caller
+    rescue ArgumentError => boom
+      raise ArgumentError, "Invalid YAML detected in #{filename}", caller
+    end 
+    return config
+end
+
+
+#loadConfig('/tmp/foo')
---
+++ tests/test_load_yamls.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+require 'test/unit'
+require 'controller'
+
+class TC_yamlTest < Test::Unit::TestCase
+#  def setup 
+#  end
+#  def teardown
+#  end
+
+  def test_file_not_found
+     assert_raise( IOError ) { loadConfig('/a_randon_and_missing_file') }
+  end
+
+  def test_bogus_file
+     bogus_filename="/var/tmp/rubytest123"
+     assert_raise( IOError ) { 
+         if File.exists?(bogus_filename)
+           File.delete(bogus_filename)
+         end
+         File.open(bogus_filename, 'w') 
+         loadConfig(bogus_filename) 
+         
+     }
+     File.delete(bogus_filename)
+  end
+  
+  def test_not_valid_yaml
+     assert_raise( ArgumentError ) { loadConfig('/etc/passwd') }
+  end
+  
+  def test_perms
+     perm_denied='/var/tmp/rubyfail'
+     assert_raise(Errno::EACCES) {
+         if File.exists?(perm_denied)
+           File.delete(perm_denied)
+         end
+         doc = "TEST: thing"
+         File.open(perm_denied, 'w') {|f| f.write(doc) }
+         File.chmod(0000, perm_denied)
+         loadConfig(perm_denied)
+     }
+     File.chmod(0644, perm_denied)
+     File.delete(perm_denied)
+  end
+  def test_all_good
+     all_good='/var/tmp/rubygood'
+     doc="TEST: thing"
+     File.open(all_good, 'w') {|f| f.write(doc) }
+     config = loadConfig(all_good)
+     assert(config['TEST'] == 'thing'  ) 
+  end
+end
