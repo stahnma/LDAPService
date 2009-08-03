4d197ea3f3730c3b8a330667d342c3483fb1e4b5
stahnma <stahnma@fedoraproject.org>
2009-08-03 16:15:31 -0500

    Adding toy mail file for now

---
+++ src/mail.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+#
+require 'net/smtp'
+
+
+def sendmail(from, to, subject, message, options = {}) 
+  #   => from_alias (optional)
+  #   =>  to_alias (optional)       
+from = options[:from_alias].to_s  + "<#{from}>"
+to = options[:to_alias].to_s  + "<#{to}>"
+msg = <<EOF
+From: #{from}
+To: #{to}
+Subject: #{subject}
+
+#{message}
+EOF
+  
+
+  Net::SMTP.start('localhost') do |smtp|
+    smtp.send_message( msg, from, to  ) 
+  end
+end
+
+
+sendmail('mike@whatever.com', 'michael.stahnke@cat.com', 'whatever', 'message body' , { :from_alias => 'mikealias' , :to_alias => "jojo"}  ) 
