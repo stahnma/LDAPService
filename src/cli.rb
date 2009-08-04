1f4ff52f79b1d99f07d6f913da3d06b41d0bbeb1
stahnma <stahnma@fedoraproject.org>
2009-08-04 18:45:28 -0500

    Using cli for debugging again.

--- src/cli.rb
+++ src/cli.rb
@@ -8,8 +8,12 @@ require 'generate_login'
 user=ENV['USER']
 password=ENV['LDAP_PASSWORD']
 
-l = LdapConnection.new
-l.login(user, password)
-#options = { 'telephoneNumber' => [" +1 309 840 1541" ] } 
-options = { 'telephoneNumber' => [ ] } 
-l.update(user, options)
+#l = LdapConnection.new
+#l.login(user, password)
+
+options = { 'telephoneNumber' => [" +1 309 840 1541" ] } 
+#options = { 'telephoneNumber' => [" +1 555 840 1541" ] } 
+adminUpdate('stahnma', options)
+#options = { 'telephoneNumber' => [ ] } 
+#l.update(user, options)
+
