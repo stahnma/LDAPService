4e59d6f89011785dd226a539d1b25da50bd34138
Michael Stahnke <mastahnke@gmail.com>
2009-08-24 23:10:49 -0500

    initial pass at the useradd script

--- src/useradd.rb
+++ src/useradd.rb
@@ -3,26 +3,18 @@ require 'ldap_utils'
 require 'utils'
 require 'generate_login'
 require 'mail'
+require 'getoptlong'
 
-user=ENV['USER']
-password=ENV['LDAP_PASSWORD']
+def usage
+  puts "#{$0} --uid <username> --email <email@example.com> --firstname <your> --lastname <mom>"
+  puts "[ --ldapuser <#{ENV['USER']}> | --ldappassword $LDAP_PASSWORD ]"
+  exit 1
+end
 
-# prompt or CLI for this crap
-username='timmy4'
-lastname='stahnke'
-firstname='michael'
-email='mastahnke@yahoo.com'
-
-# Ensure current user can login to LDAP
-config = loadConfig('../configuration.yaml')
-
-# Do some type of configuration file validation
-
-
-def addLdapUser(username, lastname, firstname, email)
+def addLdapUser(username, lastname, firstname, email, ldapuser, ldappassword)
   config = loadConfig('../configuration.yaml')
   l = LdapConnection.new
-  l.login('stahnma', ENV['LDAP_PASSWORD'])
+  l.login(ldapuser, ldappassword)
   uidNumber = l.findNextUIDNumber.to_s
   gidNumber = config['UserSetup']['DefaultGroupID'].to_s
   options = { 'cn' => [ lastname ] ,  
@@ -39,10 +31,8 @@ def addLdapUser(username, lastname, firstname, email)
            }
   dn = "uid=#{username},#{config['UserSetup']['AccountOU']},#{config['LDAPInfo']['BaseDN']}"
   l.add(dn, options)
-  sendNewUserLetter(username, lastname, firstname, email)
+#  sendNewUserLetter(username, lastname, firstname, email)
 end
-
-
 
 def sendNewUserLetter(username, lastname, firstname, email)
   userid=username
@@ -55,5 +45,53 @@ def sendNewUserLetter(username, lastname, firstname, email)
   sendemail(config['PWReset']['FromAddress'], email, "Shiney new account from #{config['Site']}", message.result(binding))
 end
 
-addLdapUser(username, lastname, firstname, email)
-#sendNewUserLetter(username, lastname, firstname, email)
+def process_options
+  account = {}
+  opts = GetoptLong.new( [ '--help', '-h', GetoptLong::NO_ARGUMENT ] ,
+    [ '--uid', '-u', GetoptLong::REQUIRED_ARGUMENT ],
+    [ '--email', '-e', GetoptLong::REQUIRED_ARGUMENT ],
+    [ '--firstname', '-f', GetoptLong::REQUIRED_ARGUMENT ],
+    [ '--lastname', '-l', GetoptLong::REQUIRED_ARGUMENT ],
+    [ '--ldapuser', '-L', GetoptLong::OPTIONAL_ARGUMENT ],
+    [ '--ldappassword', '-P', GetoptLong::OPTIONAL_ARGUMENT ]
+  )
+  begin
+  opts.each do | opt, arg|
+    case opt
+      when '--help'
+        usage
+        exit 1
+      when '--uid'
+        account[:userid] = arg.to_s
+      when '--email'
+        account[:email] = arg.to_s
+      when '--firstname'
+        account[:firstname] = arg.to_s.capitalize
+      when '--lastname'
+        account[:lastname]  = arg.to_s.capitalize
+      when '--ldapuser'
+        account[:ldapuser] = arg.to_s
+      when '--ldappassword'
+        account[:ldappassword] = arg.to_s
+      end
+  end
+  rescue GetoptLong::InvalidOption 
+      puts "Invalid command line option provided.\n\n"
+      usage
+      exit 1
+  end
+  account[:ldapuser] = ENV['USER'] unless account[:ldapuser]
+  account[:ldappassword] = ENV['LDAP_PASSWORD'] unless account[:ldappassword]
+
+  unless account[:ldappassword]
+    puts "Please set $LDAP_PASSWORD environment variable."
+    exit 1
+  end
+  return account
+end
+
+
+config = loadConfig('../configuration.yaml')
+account = process_options()
+addLdapUser(account[:userid], account[:lastname], account[:firstname], account[:email], account[:ldapuser], account[:ldappassword])
+sendNewUserLetter(account[:userid], account[:lastname], account[:firstname], account[:email])
