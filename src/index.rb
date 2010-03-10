d9530be3c0f0d63dc077de5d97b0aa342d660b9a
stahnma <stahnma@websages.com>
2010-03-10 07:04:20 -0600

    Pushing new branch

--- src/index.rb
+++ src/index.rb
@@ -4,8 +4,30 @@ require 'sinatra'
 require 'presentation'
 
 
+enable :sessions
 
-get '/' do
+get '/' do 
+  # show the default login screen
   renderfarm
 end
 
+get '/account/:name' do
+  "Hello #{params[:name]}!"
+end
+
+get '/account/:name/password/forgot' do
+  "Hello #{params[:name]} did you forget your pw?"
+end
+
+post  '/login' do 
+  session['login']  = params[:login]
+  session['password'] = params[:password]
+  "Hello #{session.inspect}"
+end
+
+get '/logout' do
+  session['login'] = nil
+  session['password'] = nil
+  
+  #unset(session['pass'])
+end
